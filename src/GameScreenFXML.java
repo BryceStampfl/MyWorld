8b5c619daac4bc72e38ccfe6e9f71d43c5ad6231
Bryce Stampfl <brycestampfl@gmail.com>
2019-11-29 09:48:06 -0800

    Overhaul of UI with FXML

---
+++ src/GameScreenFXML.java
@@ -0,0 +1,57 @@
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.util.ArrayList;
+
+
+public class GameScreenFXML{
+
+    private Stage stage;
+    private Scene scene;
+    private Group group;
+    private Rectangle2D primaryScreen;
+    private ArrayList<? extends Drawable> objectsToDraw;
+
+
+    public void init(){
+        this.group = initGroup();
+        this.scene = initScene();
+        this.stage = initStage();
+    }
+
+    private Group initGroup(){
+        group = new Group();
+        for (int i = 0; i < objectsToDraw.size(); i++) {
+            group.getChildren().add(objectsToDraw.get(i).getImageView());
+        }
+
+        return group;
+    }
+    private Scene initScene(){
+        scene = new Scene(group);
+        return scene;
+    }
+    private Stage initStage() {
+        this.primaryScreen = Screen.getPrimary().getVisualBounds();
+        stage = new Stage(StageStyle.DECORATED);
+        stage.setWidth(primaryScreen.getWidth());
+        stage.setHeight(primaryScreen.getHeight());
+        stage.setTitle("My World");
+        stage.setScene(scene);
+        return stage;
+    }
+
+    public void setDrawables(ArrayList<? extends Drawable> objectsToDraw){
+        this.objectsToDraw = objectsToDraw;
+    }
+
+    public void showScreen(){
+        stage.show();
+    }
+
+
+
+}
