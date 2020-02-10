8b834dd468967ea17a7540d233f3b58dbb973d76
Bryce Stampfl <brycestampfl@gmail.com>
2020-02-09 18:29:14 -0800

    Redesigned how Collision works, GameUnits now have an array of targets in Vision range.

--- src/ActionEvents/MoveEvent.java
+++ src/ActionEvents/MoveEvent.java
@@ -10,12 +10,19 @@ public class MoveEvent implements ActionEvent {
 
 
     public void processEvent(CombatGameUnit source){
-        source.setLocation(moveToLocation);
+        moveUtility = new MoveUtility();
+        //source.setLocation(moveToLocation);
+        //Right now the move location is the castle
+        source.setLocation(moveUtility.getPointTowardsTarget(source.getLocation(),source.getMoveLocation(),source.getWalkDistance()));
     }
 
     public MoveEvent(CombatGameUnit start, CombatGameUnit end){
         moveUtility = new MoveUtility();
          moveToLocation = moveUtility.getPointTowardsTarget(start.getLocation(), end.getLocation(), start.getWalkDistance());
+    }
+    public MoveEvent(CombatGameUnit start){
+        moveUtility = new MoveUtility();
+        moveToLocation = moveUtility.getPointTowardsTarget(start.getLocation(), start.getMoveLocation(), start.getWalkDistance());
     }
 }
 
